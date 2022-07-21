50dc9ecc24ba1f11d6e8eca4c4d6a17819e29bca
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-07-21 09:41:29 +0100

    Update script for upload as supp material

--- euphonic/publication_figures/euphonic_script_example.py
+++ euphonic/publication_figures/euphonic_script_example.py
@@ -7,23 +7,28 @@ from euphonic.plot import plot_2d
 # Read CASTEP force constants
 fc = ForceConstants.from_castep('La2Zr2O7.castep_bin')
 
-# Create array of q-points in [H,-H,-2], with H from -8 to -4
+# Create array of q-points in [H,-H,-2],
+# with H from -8 to -4
 qpts = np.zeros((1001, 3))
 qpts[:, 0] = np.linspace(-8, -4, len(qpts))
 qpts[:, 1] = -qpts[:, 0]
 qpts[:, 2] = -2
 
-# Calculate phonon frequencies and eigenvectors in [H,-H,2] direction
+# Calculate phonon frequencies and eigenvectors
+# in [H,-H,2] direction
 modes = fc.calculate_qpoint_phonon_modes(qpts)
 
-# Create DebyeWaller object at 300K on 8x8x8 Monkhorst-Pack grid
-dw_modes = fc.calculate_qpoint_phonon_modes(mp_grid([8, 8, 8]))
+# Create DebyeWaller object at 300K
+# on an 8x8x8 Monkhorst-Pack grid
+dw_modes = fc.calculate_qpoint_phonon_modes(
+    mp_grid([8, 8, 8]))
 dw = dw_modes.calculate_debye_waller(300*ureg('K'))
 
 # Calculate structure factors in [H,-H,2] with Debye-Waller
 sf = modes.calculate_structure_factor(dw=dw)
 
-# Calculate intensities using 1000 energy bins from 0 to 100 meV
+# Calculate intensities using 1000
+# energy bins from 0 to 100 meV
 ebins = np.linspace(0, 100, 1000)*ureg('meV')
 sqw = sf.calculate_sqw_map(ebins)
 
@@ -31,9 +36,10 @@ sqw = sf.calculate_sqw_map(ebins)
 sqwb = sqw.broaden(y_width=1.5*ureg('meV'))
 
 # Set x-tick labels at H=-8, -7, -6, -5 and -4
+tick_idx = np.linspace(0, len(qpts) - 1, 5, dtype=int)
 sqwb.x_tick_labels = [(idx, str(qpts[idx, 0]))
-                      for idx in np.linspace(0, len(qpts) - 1, 5, dtype=int)]
+                      for idx in tick_idx]
 
 # Plot and show figure
-fig = plot_2d(sqwb, vmax=5, y_label=f'Energy ({ebins.units:~P})')
+fig = plot_2d(sqwb, vmax=5, ylabel=f'Energy (meV)')
 fig.show()
